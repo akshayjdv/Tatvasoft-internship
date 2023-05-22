63abbd06be488c0b3fb7ad5d0cfe5c482a1175e9
AkshayJadav <96594003+akshayjdv@users.noreply.github.com>
2023-05-22 11:41:52 +0530

    Add files via upload
    day 4 of internship, learn about makeStyle of mui, useStyle hook of mui, and useState hooks. work with it and made a card to increment and decrement number by pressing a button and added styles to it by makeStyle

---
+++ day4/src/Menu.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {  NavLink } from "react-router-dom";
+import "./menu.css";
+
+const Menu = () => {
+  return (
+    <>
+      <div className="menu_style">
+        <NavLink
+          activeClassName="active_class"
+          to="/"
+          style={{ padding: "20px" }}
+        >
+          ABOUT
+        </NavLink>
+        <NavLink
+          activeClassName="active_class"
+          to="/contact"
+          style={{ padding: "20px" }}
+        >
+          CONTACT
+        </NavLink>
+        
+        <NavLink
+          activeClassName="active_class"
+          to="/name"
+          style={{ padding: "20px" }}
+        >
+          NAME
+        </NavLink>
+        
+        <NavLink
+          activeClassName="active_class"
+          to="/service"
+          style={{ padding: "20px" }}
+        >
+          SERVICE
+        </NavLink>
+      </div>
+    </>
+  );
+};
+
+export default Menu;
